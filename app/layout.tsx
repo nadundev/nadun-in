b80cf46a4063452ec36cc3a6fc1e061f8d004282
nadundev <116334152+nadundev@users.noreply.github.com>
2025-06-16 16:17:38 +0530

    Adding meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -171,6 +171,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="BuZc1879yCDqlLHuNo_2hB_wvz3tiyJdQdDzv7yNqfE" />
         <StructuredData />
       </head>
       <body
